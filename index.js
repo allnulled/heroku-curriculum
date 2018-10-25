99141ec9793ed21428cf6c538731183277195722
allnulled <todosanulados@gmail.com>
2018-10-25 12:15:56 +0200

    First endpoint with favicon.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.set('port', (process.env.PORT || 5000))
+app.use("/", express.static(__dirname + "/public"));
 app.get("/", function(request, response, next) {
 	response.send("<b>Okay...</b> We are doing good.");
 });
