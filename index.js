59abbe050f046453f27f0db0eda5dcb2db019d8f
allnulled <todosanulados@gmail.com>
2018-10-25 11:59:35 +0200

    Fourth commit.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
-
+app.set('port', (process.env.PORT || 5000))
 app.get("/", function(request, response, next) {
 	response.send("<b>Okay...</b> We are doing good.");
 });
-
-app.listen(5000);
+app.listen(app.get("port"));
